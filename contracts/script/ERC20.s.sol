361368aa68729fdb01d50ee32c2dc1f40751dc3c
DJ.Zhang <dengji@evg.co>
2024-07-15 15:28:43 +0800

    new file:   remappings.txt

---
+++ contracts/script/ERC20.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Script is Script {
+    ERC20 public token;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        token = new ERC20("Test Token", "TTT");
+    }
+}
+
+// forge script ERC20Script --rpc-url sepolia
+// forge script --broadcast ERC20Script --rpc-url sepolia
